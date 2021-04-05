8f0e6ff1bc746c8cf7c0e37a78c1ad1f727f41bd
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2021-04-04 23:18:41 -0500

    Modify shellcode script

--- dis.sh
+++ dis.sh
@@ -1,11 +1,17 @@
 
 echo "Attemptng to disassemble $1"
 
-objdump --syms $1 > $1.symbols.txt
-
-if [ -s "$1.symbols.txt" ]
+# Clear out file if it exists
+if test -f "symbols.txt"; 
 then
-    echo "Success! Table stored at $1.symbols.txt"
+    rm "symbols.txt"
+fi
+
+objdump --syms $1 > symbols.txt
+
+if [ -s "symbols.txt" ]
+then
+    echo "Success! Table stored at symbols.txt"
 else
     echo "Failed!"
 fi
