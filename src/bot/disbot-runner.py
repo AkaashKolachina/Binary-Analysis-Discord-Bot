f57a8ea66e142c1212eb1f056645d80c40afc752
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2021-04-05 21:20:05 -0500

    Add discord connection code

--- src/bot/disbot-runner.py
+++ src/bot/disbot-runner.py
@@ -1,6 +1,16 @@
 # TODO Build standard discord bot here
 # For now this may be left empty
-def main()
+import os
+import discord
+
+TOKEN = ...
+client = discord.Client()
+@client.event
+async def on_ready():
+    print("Pong!")
+client.run(TOKEN)
+
+def main():
     print("Discord bot running!")
 
 if __name__ == '__main__':
