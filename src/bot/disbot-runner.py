9653ac220fee40df41aa1de6c73617571cba33dd
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2021-04-10 16:34:02 -0500

    Establish connection

--- src/bot/disbot-runner.py
+++ src/bot/disbot-runner.py
@@ -1,14 +1,5 @@
 # TODO Build standard discord bot here
 # For now this may be left empty
-import os
-import discord
-
-TOKEN = ...
-client = discord.Client()
-@client.event
-async def on_ready():
-    print("Pong!")
-client.run(TOKEN)
 
 def main():
     print("Discord bot running!")
--- src/run.py
+++ src/run.py
@@ -1,8 +1,26 @@
 # Imports Here
 # This is the primary runner function that will get all your scripts running
+import os
+import discord
+import tokens
+
+from discord.ext import commands
 
 def main():
-	print("This is where your main function will go")
+    TOKEN = tokens.DISCORD_TOKEN
+    '''
+    client = discord.Client()
+    @client.event
+    async def on_ready():
+        print("Discord bot running!")
+    client.run(TOKEN)
+    '''
+    #bot = discord.Client()
+    bot = commands.Bot(command_prefix='!')
+    @bot.command(name='ping', pass_context = True)
+    async def pong(ctx):
+	    await ctx.send("pong!")
+    bot.run(TOKEN)
 
 if __name__ == '__main__':
-	main()
+    main()
