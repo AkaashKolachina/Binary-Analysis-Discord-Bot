4b8a3e2d02f58eb64b84c5b57875d89598138b04
AkaashKolachina <54248406+AkaashKolachina@users.noreply.github.com>
2021-04-20 03:42:48 -0500

    Implement test function to download a given executable

--- src/run.py
+++ src/run.py
@@ -8,18 +8,19 @@ from discord.ext import commands
 
 def main():
     TOKEN = tokens.DISCORD_TOKEN
-    '''
-    client = discord.Client()
-    @client.event
-    async def on_ready():
-        print("Discord bot running!")
-    client.run(TOKEN)
-    '''
-    #bot = discord.Client()
+
     bot = commands.Bot(command_prefix='!')
     @bot.command(name='ping', pass_context = True)
     async def pong(ctx):
 	    await ctx.send("pong!")
+    
+
+    @bot.command(name = 'download', pass_context = True)
+    async def download(ctx):
+        download_path = '/Users/akaashkolachina/UIUC2020/Binary-Analysis-Discord-Bot/src/test-files/current_file'
+        await ctx.message.attachments[0].save(fp = download_path)
+
+        
     bot.run(TOKEN)
 
 if __name__ == '__main__':
